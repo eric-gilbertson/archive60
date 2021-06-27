bb4097e01e768b398e69ec23b69acb36fb5a6f99
Eric Gibertson <eric_gilbertson@yahoo.com>
2021-06-26 21:42:49 -0700

    skip ffmpeg check in files < 4MB.

--- find_silence.py
+++ find_silence.py
@@ -2,8 +2,10 @@
 
 import math, datetime, subprocess, sys, getopt, os, shutil
 import glob
+from calendar import monthrange
 
 base_path = '/home/ericg/mybook_archive/main_backup/bu1'
+#base_path = '/Users/Barbara/src/archive60/archives'
 
 def parse_args(argv):
    global start_date, is_today
@@ -37,10 +39,16 @@ def execute_cmd(cmd):
         sys.exit()
 
 def check_for_silence(file):
-    cmd = 'ffmpeg -i {}  -af silencedetect=n=-50dB:d=120.0 -f null -'.format(file)
-    (output, err)  = execute_cmd(cmd)
-    silence_start_idx = err.find('silence_start:')
-    silence_end_idx = err.find('silence_end:')
+    silence_end_idx = -1
+    size = os.path.getsize(file)
+    # completely silent files are < 4MB
+    if size <= 4000000:
+        silence_start_idx = 1 # aribtrary value to mark as silent
+    else:
+        cmd = 'ffmpeg -i {}  -af silencedetect=n=-50dB:d=120.0 -f null -'.format(file)
+        (output, err)  = execute_cmd(cmd)
+        silence_start_idx = err.find('silence_start:')
+        silence_end_idx = err.find('silence_end:')
 
     if silence_start_idx >= 0:
         if silence_end_idx == -1:
@@ -57,22 +65,42 @@ def check_for_silence(file):
             print(file + ': partial ' + suffix)
             shutil.move(file, file + suffix)
 
+def make2digit(some_int):
+    res = str(some_int)
+    if some_int < 10:
+        res = '0' + res
+
+    return res
+
+def process_day(year, month, day):
+    day_path = "{}-{}-{}".format(year, make2digit(month), make2digit(day))
+    print("Process day {}".format(day_path))
+    src_path = '{}/{}/{}/kzsu_archive*.mp3'.format(base_path, year, day_path)
+    files = glob.glob(src_path)
+    for file in files:
+        check_for_silence(file)
+
+def process_month(year, month):
+    num_days = monthrange(year, month)[1]
+    for day in range(num_days):
+        process_day(year, month, day+1)
+
+def process_year(year):
+    for month in range(12):
+        process_month(year, month+1)
+
 parse_args(sys.argv[1:])
 date_ar = start_date.split('-')
 date_len = len(date_ar)
+year = int(date_ar[0])
 
-year = date_ar[0]
 if date_len == 3:
-    src_path = '{}/{}/{}/*.mp3'.format(base_path, year, start_date)
+    process_day(year, int(date_ar[1]), int(date_ar[2]))
 elif date_len == 2:
-    src_path = '{}/{}/{}-{}*/*.mp3'.format(base_path, year, year, date_ar[1])
+    process_month(year, int(date_ar[1]))
 elif date_len == 1:
-    src_path = '{}/{}/*/*.mp3'.format(base_path, year, date_ar[1])
+    process_year(year)
 
-print('src: ' + src_path)
 
-files = glob.glob(src_path)
-for file in files:
-    check_for_silence(file)
 
 
