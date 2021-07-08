b4e346c1bbe24454e936d09a3fb7a28a66a28db5
eric gilbertson <ericg@kzsu.stanford.edu>
2021-07-07 22:42:07 -0700

    enhance logging

--- archive60.py
+++ archive60.py
@@ -1,4 +1,4 @@
-##!/usr/bin/python
+#!/usr/bin/python
 #
 # This file converts the 15 minute archive files created by the old podkeeper archiver
 # to the new 60 minute files that start on the hour. The source files are stored under
@@ -11,7 +11,7 @@
 #    - source files are incomplete, e.g. there are time gaps
 #    - segment start time offset drifts over time
 #
-import datetime, subprocess, sys, getopt, os
+import datetime, subprocess, sys, getopt, os, math, shutil
 from calendar import monthrange
 
 from array import *
@@ -21,47 +21,56 @@ import urllib
 
 start_date = ''
 create_files = False
+time_skew = 0 # offset used to get to TOTH
+
 SRC_PATH = '/home/ericg/mybook_archive/main_backup/bu1'
 #SRC_PATH = './archives'
-DEST_PATH = './archive_files'
+DEST_PATH = '/media/pr2100/kzsu-aircheck-archives'
+#DEST_PATH = './archive_files'
 
 SEG_LEN_MINS = 15
 SEG_LEN_SECS = SEG_LEN_MINS * 60
 
+def log_it(msg):
+   print(msg, flush=True)
+
 def parse_args(argv):
-   global start_date, is_today, create_files
+   global start_date, create_files, time_skew
 
    try:
-      opts, args = getopt.getopt(argv,"d:c",["date","create_files"])
+      opts, args = getopt.getopt(argv,"d:c:s:",["date","create_files","time-skew"])
    except getopt.GetoptError:
-      print ('test.py -c -d YYYY-MM-DD')
-      sys.exit(2)
+      log_fatal ('test.py -c -d YYYY-MM-DD -s +/-<0-60>')
 
    for opt, arg in opts:
       if opt == '-h':
-         print ('test.py -c -date YYYY-MM-DD')
+         log_it ('test.py -c -date YYYY-MM-DD')
          sys.exit()
       elif opt in ("-d", "--date"):
          start_date = arg
       elif opt in ("-c", "--create-files"):
          create_files = True
+      elif opt in ("-s", "--time-skew"):
+         time_skew  = int(arg.strip())
+         if abs(time_skew) > 59:
+             log_fatal("invalid time skew: {}, {}".format(arg, time_skew))
 
-   print ('Show date: {}'.format(start_date))
+   log_it ('Show args: {}, {}'.format(start_date, time_skew))
 
 # return time length of an mp3 file using ffmpeg or -1 if invalid.
 # assumes user has ffmpeg in PATH.
 def execute_cmd(cmd):
     try:
-        print("Execute: +{}+\n".format(cmd))
         p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
         (output, err) = p.communicate()
         p_status = p.wait()
-        err = str(err)
-        print("Returned +{}+, +{}+\n".format(output, str(err)))
-        sys.exit() ########
+        if p.returncode != 0:
+            err = str(err)
+            log_it("Execute: +{}+\n".format(cmd))
+            log_fatal("Returned +{}+, +{}+\n".format(output, str(err)))
     except Exception as ioe:
-        print('Exception getting duration for: {}, {}'.format(cmd, ioe))
-        sys.exit()
+        log_it("Execute: +{}+\n".format(cmd))
+        log_fatal('Exception getting duration for: {}, {}'.format(cmd, ioe))
 
 def get_file_path(year_int, month_int, day_int):
     file_path = '{}/{}/kzsu_archive_{}{}{}*'.format(SRC_PATH, year, year, make2digit(month), make2digit(day))
@@ -138,7 +147,7 @@ def make2digit(some_int):
     return res
 
 def log_fatal(msg):
-    print(msg)
+    log_it(msg)
     sys.exit()
 
 # check that the parameters for the audio file are correct, e.g:
@@ -148,7 +157,7 @@ def check_audio_validity(concat_files, time_offset_mins):
     if concat_len < 4 or concat_len > 5:
         log_fatal('incorrect file count: ' + concat_files[0])
     elif time_offset_mins < 0 or time_offset_mins > 14:
-        log_fatal('invalid time offset: ' + time_offset_mins)
+        log_fatal('invalid time offset: ' + str(time_offset_mins))
 
     first_file = concat_files[0]
     file_minutes = get_file_minutes(first_file)
@@ -177,6 +186,7 @@ def check_audio_validity(concat_files, time_offset_mins):
     return True
 
 def create_audio_file(concat_files, time_offset_mins_int):
+    global time_skew
     months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
     # will exit if invalid params
@@ -190,8 +200,10 @@ def create_audio_file(concat_files, time_offset_mins_int):
     hour = date.hour
     time_offset_opt = ''
     if time_offset_mins_int != 0:
-        time_offset = make2digit(time_offset_mins_int)
-        time_offset_opt = ' -ss 00:{}:00 '.format(time_offset)
+        total_seconds = (time_offset_mins_int * 60) + time_skew
+        mins = math.floor(total_seconds / 60)
+        seconds = total_seconds % 60
+        time_offset_opt = ' -ss 00:{}:{} '.format(make2digit(mins), make2digit(seconds))
 
     outdir = '{}/{}/{}/{}/'.format(DEST_PATH, year,  months[month-1], make2digit(day))
     if not os.path.exists(outdir):
@@ -199,9 +211,16 @@ def create_audio_file(concat_files, time_offset_mins_int):
 
     outfile = 'kzsu-{}-{}-{}-{}00.mp3'.format(year, make2digit(month), make2digit(day), make2digit(hour))
     concat_str = '|'.join(concat_files)
-    cmd = '/usr/local/bin/ffmpeg -hide_banner -y {} -t 3600 -i "concat:{}" -acodec copy {}/{}'.format(time_offset_opt, concat_str, outdir, outfile)
-    print("ffmpeg cmd: " + cmd)
+    outpath = '{}/{}'.format(outdir, outfile)
+    cmd = '/usr/local/bin/ffmpeg -hide_banner -y {} -t 3600 -i "concat:{}" -acodec copy {}'.format(time_offset_opt, concat_str, outpath)
+    log_it("ffmpeg cmd: " + cmd)
     if create_files:
+        if os.path.exists(outpath):
+           suffix = datetime.datetime.now().strftime(".%Y-%m-%dT%H:%M:%S.backup")
+           # only move the original file to backup
+           if not os.path.exists(outpath + suffix):
+               shutil.move(outpath, outpath + suffix)
+
         execute_cmd(cmd)
 
 def is_valid_start_time(file):
@@ -210,14 +229,14 @@ def is_valid_start_time(file):
     return is_valid
 
 def process_day(year, month, day):
-    print("Process day {}, {}, {}".format(year, month, day))
+    log_it("Process day {}, {}, {}".format(year, month, day))
     files = get_src_files(year, month, day)
     prev_file = get_prev_file(year, month, day)
     if prev_file:
         files.insert(0, prev_file)
 
     if len(files) == 0:
-        print("no files for: {}-{}-{}".format(year, month, day))
+        log_it("no files for: {}-{}-{}".format(year, month, day))
         return
 
     concat_files = ''
@@ -247,16 +266,16 @@ def process_day(year, month, day):
                 concat_files.append(file)
 
 def process_month(year, month):
-    print("Process month {}, {}".format(year, month))
+    log_it("Process month {}, {}".format(year, month))
     num_days = monthrange(year, month)[1]
     for day in range(num_days):
-        print('process day: ' + str(day+1))
+        log_it('process day: ' + str(day+1))
         process_day(year, month, day+1)
 
 def process_year(year):
-    print("Process year {}".format(year))
+    log_it("Process year {}".format(year))
     for month in range(12):
-        print("process month: " + month+1)
+        log_it("process month: " + month+1)
         process_month(year, month+1)
 
 parse_args(sys.argv[1:])
@@ -270,7 +289,7 @@ elif date_len == 2:
 elif date_len == 3:
     process_day(int(date_ar[0]), int(date_ar[1]), int(date_ar[2]))
 else:
-    print("Invalid date: " + start_date)
+    log_it("Invalid date: " + start_date)
 
 
 
